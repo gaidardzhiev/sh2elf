39e17f2507c604adfba1a88cc659e5957934bba6
gaidardzhiev <1v4n@protonmail.com>
2025-11-27 09:33:12 +0200

    init

---
+++ scripts/logic.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# validate comments and conditional execution operators
+/bin/true && echo first && /bin/false || echo after-false
+/bin/false && echo unreachable-and
+/bin/false || echo fallback-two
+/bin/true || echo unreachable-or
+echo inline # inline comment at end of command
+   # comment with leading spaces
